4445fb2b05ce597cf93dc01a4322ce71621d6479
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-22 15:23:02 -0600

    adding colors

--- helpers/msg.js
+++ helpers/msg.js
@@ -11,7 +11,13 @@ const showMenu = () => {
     console.log(' Select an option'.green);
     console.log('===============================\n'.green);
     //creating a menu
-    console.log();
+    console.log(`${ '1.'.brightGreen } Set work`);
+    console.log(`${ '2.'.brightGreen } Order work`);
+    console.log(`${ '3.'.brightGreen } Order work completed`);
+    console.log(`${ '4.'.brightGreen } Order pending's work`);
+    console.log(`${ '5.'.brightGreen } Complete work(s)`);
+    console.log(`${ '6.'.brightGreen } Delete work`);
+    console.log(`${ '0.'.brightGreen } Exit\n`);
 }
 
 module.exports = {
