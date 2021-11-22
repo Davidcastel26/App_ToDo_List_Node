b9f7a88fc745e646f1f907082b385e6a7c2e2ac2
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-22 17:00:47 -0600

    adding menu done

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ console.clear()
 const main = async() => {
     console.log('hola mundo');
 
-    showMenu()
+    let opt = ''
+
+    do{
+       opt = await showMenu();
+       console.log( opt );
+       if(opt !== '0' ) await pause()
+    }while(opt !== '0')
     // pause()
 }
 
--- helpers/msg.js
+++ helpers/msg.js
@@ -1,12 +1,10 @@
 require('styled-components');
-
-require('styled-components');
-
 require('colors');
 
 const showMenu = () => {
-
-    console.clear();
+    
+  return new Promise( resolve => {
+        console.clear();
     console.log('==============================='.green);
     console.log(' Select an option'.green);
     console.log('===============================\n'.green);
@@ -27,11 +25,14 @@ const showMenu = () => {
     readline.question('Select an option now: ', (opt) => {
         // console.log({opt});
         readline.close();
+        resolve(opt)
     })
+  })
 }
 
 //creating a stop for the app and try to 
 const pause = () => {
+  return new Promise( resolve => {
     const readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
@@ -39,7 +40,9 @@ const pause = () => {
 
     readline.question(`\n Press ${'ENTER'.magenta} to continue \n`, (opt) => {
         readline.close();
+        resolve();
     })
+  })
 }
 
 module.exports = {
